975b3238820a38352fb370515e11b9ddc6b7a1d7
amarciniak25 <a@amarciniak.com>
2015-10-12 20:07:48 -0400

    All realys on

---
+++ system/all-relay-on.php
@@ -0,0 +1,11 @@
+<?php
+  #This scipt turns all relays on
+  include 'relay-01-on.php';
+  include 'relay-02-on.php';
+  include 'relay-03-on.php';
+  include 'relay-04-on.php';
+  include 'relay-05-on.php';
+  include 'relay-06-on.php';
+  include 'relay-07-on.php';
+  include 'relay-08-on.php';
+?>
