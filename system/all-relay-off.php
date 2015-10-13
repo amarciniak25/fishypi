6e9b2c0def22634392f6ffd8af74f9f4c5631696
amarciniak25 <a@amarciniak.com>
2015-10-12 20:11:02 -0400

    All relay off

---
+++ system/all-relay-off.php
@@ -0,0 +1,11 @@
+<?php
+  #This scipt turns all relays off
+  include 'relay-01-off.php';
+  include 'relay-02-off.php';
+  include 'relay-03-off.php';
+  include 'relay-04-off.php';
+  include 'relay-05-off.php';
+  include 'relay-06-off.php';
+  include 'relay-07-off.php';
+  include 'relay-08-off.php';
+?>
