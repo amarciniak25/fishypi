cb03b275e866eaf9bc99632fef729a40856bcb46
yatrase <yatrasevich@gmail.com>
2015-11-08 21:14:08 -0500

    Fixed save schedule

--- saveschedule.php
+++ saveschedule.php
@@ -1,28 +1,28 @@
 <?php
 $OUTPUT_FILENAME = "Data/Schedule.txt";
-$schedule1 = $_POST['scgedule1'];
-$schedule2 = $_POST['scgedule2'];
-$schedule3 = $_POST['scgedule3'];
-$schedule4 = $_POST['scgedule4'];
-$schedule5 = $_POST['scgedule5'];
-$schedule6 = $_POST['scgedule6'];
-$schedule7 = $_POST['scgedule7'];
-$schedule8 = $_POST['scgedule8'];
+$schedule1 = $_POST['schedule1'];
+$schedule2 = $_POST['schedule2'];
+$schedule3 = $_POST['schedule3'];
+$schedule4 = $_POST['schedule4'];
+$schedule5 = $_POST['schedule5'];
+$schedule6 = $_POST['schedule6'];
+$schedule7 = $_POST['schedule7'];
+$schedule8 = $_POST['schedule8'];
 
-file_put_contents($OUTPUT_FILENAME, $schedule1);
-file_put_contents($OUTPUT_FILENAME, $schedule2, FILE_APPEND);
-file_put_contents($OUTPUT_FILENAME, $schedule3, FILE_APPEND);
-file_put_contents($OUTPUT_FILENAME, $schedule4, FILE_APPEND);
-file_put_contents($OUTPUT_FILENAME, $schedule5, FILE_APPEND);
-file_put_contents($OUTPUT_FILENAME, $schedule6, FILE_APPEND);
-file_put_contents($OUTPUT_FILENAME, $schedule7, FILE_APPEND);
+file_put_contents($OUTPUT_FILENAME, $schedule1 . " ");
+file_put_contents($OUTPUT_FILENAME, $schedule2 . " ", FILE_APPEND);
+file_put_contents($OUTPUT_FILENAME, $schedule3 . " ", FILE_APPEND);
+file_put_contents($OUTPUT_FILENAME, $schedule4 . " ", FILE_APPEND);
+file_put_contents($OUTPUT_FILENAME, $schedule5 . " ", FILE_APPEND);
+file_put_contents($OUTPUT_FILENAME, $schedule6 . " ", FILE_APPEND);
+file_put_contents($OUTPUT_FILENAME, $schedule7 . " ", FILE_APPEND);
 file_put_contents($OUTPUT_FILENAME, $schedule8, FILE_APPEND);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="refresh" content="0;url=./ManualControl.php">
+<meta http-equiv="refresh" content="0;url=./Schedule.php">
 </head>
 <body></body>
 
